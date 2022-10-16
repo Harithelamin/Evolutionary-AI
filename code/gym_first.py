04526ae07f008b17601fffb22376d8f3996b7f3c
Harithelamin <harithelamin@gmail.com>
2022-10-16 06:42:17 +0200

    Update gym_first.py

--- code/gym_first.py
+++ code/gym_first.py
@@ -1,18 +1,26 @@
 #https://www.gymlibrary.dev/
 #pip install gym
 #pip install --upgrade pip
+#pip install gym[box2d]
+#pip uninstall gym
+#pip install gym[box2d]
+#python.exe -m pip install --upgrade pip
+#pip install gym[box2d] pygame      
+
+from os import environ      
+from gym.utils.env_checker import check_env
+check_env(environ)
 
 import gym
-env = gym.make('CartPole-v0')
-
-from email import policy
-
 env = gym.make("LunarLander-v2", render_mode="human")
-observation, info = env.reset(seed=42)
-for _ in range(1000):
-   action = policy(observation)  # User-defined policy function
-   observation, reward, terminated, truncated, info = env.step(action)
+env.action_space.seed(42)
 
-   if terminated or truncated:
-      observation, info = env.reset()
-env.close()
+observation, info = env.reset(seed=42)
+
+for _ in range(1000):
+    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()   
